8ce62439788876b8fb9a94aef9c2df9d18ad974d
Evelyn Kosma <ezranaja@gmail.com>
2021-03-12 18:41:04 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,13 @@
 class Person
   def name=name
     @name = name
+<<<<<<< HEAD
   end
   def job=job
     @job = job
   end
+=======
+>>>>>>> 761e5b1c69bb21ec759abb4d626431ab32820750
   def name
     @name
   end
